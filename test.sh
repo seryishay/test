d811ca550c6cf11f07cdd48baca480ffc1edbce1
yishay.seroussi <yishay.seroussi@axonius.com>
2021-05-25 16:10:40 +0300

    -

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ python3 -m pytest --all -v --junitxml="/tmp/junit-result.xml"
 
 export ANNOTATE_FAILED_FILENAME=annotate_fail.md
 export ANNOTATE_PASSED_FILENAME=annotate_passed.md
-export ARTIFACTS_DIR=./tmp
+export ARTIFACTS_DIR=/tmp
 EXIT_CODE=0
 python3 junit_annotate.py || EXIT_CODE="@?"
 < $ANNOTATE_FAILED_FILENAME buildkite-agent annotate --style 'error' --context 'ctx-error'
