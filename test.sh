7bfadaaf2d226e53660015518cde10acfaba51a3
yishay.seroussi <yishay.seroussi@axonius.com>
2021-05-25 15:58:02 +0300

    -

--- test.sh
+++ test.sh
@@ -1,2 +1,11 @@
 pip3 install pytest
 python3 -m pytest --all -v --junitxml="/tmp/junit-result.xml"
+
+export ANNOTATE_FAILED_FILENAME=annotate_fail.md
+export ANNOTATE_PASSED_FILENAME=annotate_passed.md
+export ARTIFACTS_DIR=.
+EXIT_CODE=0
+python3 .buildkite/junit_annotate.py || EXIT_CODE="@?"
+< $ANNOTATE_FAILED_FILENAME buildkite-agent annotate --style 'error' --context 'ctx-error'
+< $ANNOTATE_PASSED_FILENAME buildkite-agent annotate --style 'success' --context 'ctx-success'
+exit EXIT_CODE
